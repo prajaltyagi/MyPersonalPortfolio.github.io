de7021b26d005a7107f1370509461969b048127c
prajal_tygai <121673589+prajaltyagi@users.noreply.github.com>
2023-02-25 18:27:36 -0800

    MypersonalPortfolio

---
+++ script.js
@@ -0,0 +1,46 @@
+
+    let menu1=document.getElementById('menu1')
+    let h1=document.getElementById('h1')
+    let close1=document.getElementById('close1')
+
+    menu1.onclick=function(){
+        h1.style.display="block"
+    }
+    close1.onclick=function(){
+        h1.style.display="none"
+    }
+    
+    let menu2=document.getElementById('menu2')
+    let h2=document.getElementById('h2')
+    let close2=document.getElementById('close2')
+
+    menu2.onclick=function(){
+        h2.style.display="block"
+    }
+    close2.onclick=function(){
+        h2.style.display="none"
+    }
+
+    
+    let menu4=document.getElementById('menu4')
+    let h4=document.getElementById('h4')
+    let close4=document.getElementById('close4')
+
+    menu4.onclick=function(){
+        h4.style.display="block"
+    }
+    close4.onclick=function(){
+        h4.style.display="none"
+    }
+
+    let menu5=document.getElementById('menu5')
+    let h5=document.getElementById('h5')
+    let close5=document.getElementById('close5')
+
+    menu5.onclick=function(){
+        h5.style.display="block"
+    }
+    close5.onclick=function(){
+        h5.style.display="none"
+    }
+
